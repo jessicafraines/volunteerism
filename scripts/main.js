11d7e9229a52497c36afa4db0f660bfed6c503b1
jessicafraines <jessicafraines@gmail.com>
2014-10-17 14:18:58 -0500

    Random student complete, but compiles list

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 var students = ['bob', 'joe', 'sally', 'dakota', 'brian', 'dave', 'jessica', 'pat', 'gary', 'janice'];
+var student;
 
 document.addEventListener('DOMContentLoaded', function(){
   var $button = document.querySelector('button');
@@ -7,7 +8,24 @@ document.addEventListener('DOMContentLoaded', function(){
   $button.addEventListener('click', function(){
     if ($select.value === 'randomStudent'){
       var s = Math.floor((Math.random() * 10) + 1);
-      alert(students[s]);
+      student = students[s];
+      var $target = document.querySelector('.target');
+      $target.value = '';
+      var docFragment = createPTag();
+      $target.appendChild(docFragment);
     }
   });
 });
+
+function createPTag(){
+    var docFragment = document.createDocumentFragment();
+    var $div        = document.createElement('div');
+    $div.setAttribute('class', 'myClass');
+    var $p          = document.createElement('p');
+    $p.textContent  = student;
+    $div.appendChild($p);
+
+    docFragment.appendChild($div);
+
+    return docFragment;
+}
