4fc8ddd5916e74d07dcf54f98fb32d7e85d7f14b
jessicafraines <jessicafraines@gmail.com>
2014-10-17 11:46:06 -0500

    Added random student alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
+var students = ['bob', 'joe', 'sally', 'dakota', 'brian', 'dave', 'jessica', 'pat', 'gary', 'janice'];
+
 document.addEventListener('DOMContentLoaded', function(){
   var $button = document.querySelector('button');
   var $select = document.querySelector('select');
 
   $button.addEventListener('click', function(){
-    alert($select.value);
+    if ($select.value === 'randomStudent'){
+      var s = Math.floor((Math.random() * 10) + 1);
+      alert(students[s]);
+    }
   });
 });
